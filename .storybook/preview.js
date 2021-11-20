435a10eebaf39c1a686b289443c1894ddbe91b96
Balaji <balaji3049@gmail.com>
2021-11-20 08:57:02 +0530

    Added react material-ui and storybook libraries to the project

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+/* export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+} */
+
+import React from "react";
+
+export const decorators = [
+    (Story) => (
+        <div /* style={{ margin: "3em" }} */>
+            <Story />
+        </div>
+    ),
+];
