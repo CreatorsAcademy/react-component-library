52f5cb2db6bae43fe3626d8f671f10b68c48172e
Balaji <balaji3049@gmail.com>
2021-11-22 16:37:08 +0530

    Added dropdown component to the library

---
+++ src/components/Dropdown/index.jsx
@@ -0,0 +1,43 @@
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+function Dropdown(props) {
+    return (
+        <Autocomplete
+            multiple
+            id="checkboxes-tags-demo"
+            options={props.options}
+            disableCloseOnSelect
+            getOptionLabel={(option) => option.title}
+            renderOption={(props, option, { selected }) => {
+                return (
+                    <li {...props}>
+                        <Checkbox
+                            icon={icon}
+                            checkedIcon={checkedIcon}
+                            style={{ marginRight: 8 }}
+                            checked={selected}
+                        />
+                        {option.title}
+                    </li>
+                );
+            }}
+            style={{ width: 500 }}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label={props.label}
+                    placeholder={props.placeholder || null}
+                />
+            )}
+        />
+    );
+}
+
+export default Dropdown;
