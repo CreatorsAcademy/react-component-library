be6400a8b401694307db2d641127c4c57e61d723
Balaji <balaji3049@gmail.com>
2021-12-11 11:32:40 +0530

    Corrected component mapping in app js file .....

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Checkbox label="Actions" name="actions" />
+            <div>Import components here</div>
         </ThemeProvider>
     );
 }
