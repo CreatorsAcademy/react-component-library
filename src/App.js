b01e843f86bb02929d81d12dfae6654f58baa76e
Balaji <balaji3049@gmail.com>
2021-12-11 11:32:59 +0530

    Corrected component mapping in app js file .....

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { ThemeProvider, styled } from "@mui/material/styles";
 import theme from "./theme";
 import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import TextField from "@mui/material/TextField";
-import Stack from "@mui/material/Stack";
-import Checkbox from "./components/Checkbox";
 
 function App() {
     return (
